6c106a6d4d79b3998c1c6a57d2ba3c200a9faa4c
Richard <essence-barons.03@icloud.com>
2022-06-16 11:05:37 +0100

    init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using test.Models;
+
+namespace test.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult CreatePerson()
+    {
+
+        var model = new PersonViewModel
+        {
+            Name = "John Smith",
+            Email = "joh.smith1234@icloud.com",
+            Hobbies = new List<Hobby>
+            {
+                new Hobby {Name = "Football", Type = HobbyType.Outdoor},
+                new Hobby {Name = "Swimming", Type = HobbyType.Indoor},
+                new Hobby {Name = "Chess", Type = HobbyType.Indoor},
+                new Hobby {Name = "Rugby", Type = HobbyType.Outdoor},
+            }
+        };
+
+
+        return View(model);
+    }
+
+    [HttpPost]
+    public IActionResult CreatePerson(PersonViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            // something went wrong
+            return View(model);
+        }
+
+        // Save to database...
+
+        return RedirectToAction("Index");
+    }
+
+
+
+
+
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ Models/PersonViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace test.Models
+{
+	public class PersonViewModel
+	{
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+        public List<Hobby> Hobbies { get; set; } = new List<Hobby>();
+    }
+
+    public class Hobby
+    {
+        public string Name { get; set; }
+
+        public HobbyType Type { get; set; }
+    }
+
+    public enum HobbyType
+    {
+        Indoor,
+        Outdoor
+    }
+}
+
